875d0c8a4c0aec1114cc8972f20d3d0ffe138816
Joaquin <joaquin@pop-os.localdomain>
2021-12-15 11:54:20 -0300

    Ecommerce

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Announcement from "../components/Announcment"
+import Categories from "../components/Categories"
+import Navbar from "../components/Navbar"
+import Products from "../components/Products"
+import Slider from "../components/Slider"
+
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+    </div>
+  )
+}
+
+export default Home
